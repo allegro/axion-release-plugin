d918ca3c18d97756618d343ef3571cc9a5e5b9c6
Szczepan Faber <s.faber@airbnb.com>
2021-03-24 10:49:04 -0500

    Exposed getPreviousVersion() API

--- src/main/java/pl/allegro/tech/build/axion/release/domain/VersionService.java
+++ src/main/java/pl/allegro/tech/build/axion/release/domain/VersionService.java
@@ -38,7 +38,8 @@ public class VersionService {
         }
 
 
-        return new DecoratedVersion(versionContext.getVersion().toString(), finalVersion, versionContext.getPosition());
+        return new DecoratedVersion(versionContext.getVersion().toString(), finalVersion, versionContext.getPosition(),
+            versionContext.getPreviousVersion().toString());
     }
 
     public static class DecoratedVersion {
@@ -46,11 +47,14 @@ public class VersionService {
         private final String undecoratedVersion;
         private final String decoratedVersion;
         private final ScmPosition position;
+        private final String previousVersion;
 
-        public DecoratedVersion(String undecoratedVersion, String decoratedVersion, ScmPosition position) {
+        public DecoratedVersion(String undecoratedVersion, String decoratedVersion, ScmPosition position,
+                                String previousVersion) {
             this.undecoratedVersion = undecoratedVersion;
             this.decoratedVersion = decoratedVersion;
             this.position = position;
+            this.previousVersion = previousVersion;
         }
 
         @Input
@@ -66,6 +70,11 @@ public class VersionService {
         @Nested
         public final ScmPosition getPosition() {
             return position;
+        }
+
+        @Nested
+        public final String getPreviousVersion() {
+            return previousVersion;
         }
     }
 }
