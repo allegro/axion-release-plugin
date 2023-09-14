495031b798e5020175b04651eaac03c7d17347ee
shashken <shashken@microsoft.com>
2023-09-14 15:21:27 +0300

    As set

--- src/test/groovy/pl/allegro/tech/build/axion/release/infrastructure/git/GitRepositoryTest.groovy
+++ src/test/groovy/pl/allegro/tech/build/axion/release/infrastructure/git/GitRepositoryTest.groovy
@@ -524,7 +524,7 @@ class GitRepositoryTest extends Specification {
         repository.commit([fileInB], 'Add file bar in subdirB')
 
         when:
-        ScmPosition position = repository.positionOfLastChangeIn('a\\aa', [], [])
+        ScmPosition position = repository.positionOfLastChangeIn('a\\aa', [], [].toSet())
 
         then:
         position.revision == headSubDirAChanged
@@ -552,7 +552,7 @@ class GitRepositoryTest extends Specification {
         commitFile('after/aa', 'after')
 
         when:
-        ScmPosition position = repository.positionOfLastChangeIn(importantDir, [], [])
+        ScmPosition position = repository.positionOfLastChangeIn(importantDir, [], [].toSet())
 
         then:
         position.revision == headSubDirAChanged
