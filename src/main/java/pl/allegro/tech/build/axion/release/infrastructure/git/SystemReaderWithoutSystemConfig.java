72d5272102d917cae72dc1d39a668adbf170685b
Miłosz Kałucki <milosz.kalucki@allegro.com>
2023-11-28 12:19:46 +0100

    Use a dedicated SystemReader preventing JGit from using system config

---
+++ src/main/java/pl/allegro/tech/build/axion/release/infrastructure/git/SystemReaderWithoutSystemConfig.java
@@ -0,0 +1,73 @@
+package pl.allegro.tech.build.axion.release.infrastructure.git;
+
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.util.FS;
+import org.eclipse.jgit.util.SystemReader;
+
+public class SystemReaderWithoutSystemConfig extends SystemReader
+{
+    private static final SystemReader DefaultSystemReader = SystemReader.getInstance();
+
+    @Override
+    public String getenv(String variable)
+    {
+        return DefaultSystemReader.getenv(variable);
+    }
+
+    @Override
+    public String getHostname()
+    {
+        return DefaultSystemReader.getHostname();
+    }
+
+    @Override
+    public String getProperty(String key)
+    {
+        return DefaultSystemReader.getProperty(key);
+    }
+
+    @Override
+    public long getCurrentTime()
+    {
+        return DefaultSystemReader.getCurrentTime();
+    }
+
+    @Override
+    public int getTimezone(long when)
+    {
+        return DefaultSystemReader.getTimezone(when);
+    }
+
+    @Override
+    public FileBasedConfig openUserConfig(Config parent, FS fs)
+    {
+        return DefaultSystemReader.openUserConfig(parent, fs);
+    }
+
+    @Override
+    public FileBasedConfig openJGitConfig(Config parent, FS fs) {
+        return DefaultSystemReader.openJGitConfig(parent, fs);
+    }
+
+    // Return an empty system configuration to prevent JGit from accessing it
+    // This resolves issues with Gradle being unable to save configuration cache
+    // Based on https://stackoverflow.com/a/59110721
+    @Override
+    public FileBasedConfig openSystemConfig(Config parent, FS fs)
+    {
+        return new FileBasedConfig(parent, null, fs)
+        {
+            @Override
+            public void load()
+            {
+            }
+
+            @Override
+            public boolean isOutdated()
+            {
+                return false;
+            }
+        };
+    }
+}
