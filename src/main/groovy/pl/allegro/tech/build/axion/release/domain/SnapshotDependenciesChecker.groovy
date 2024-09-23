0e15a626e2ed6c0527f851bb3d71ad4e7bdda36f
Mike Sou <33867885+imvs@users.noreply.github.com>
2024-09-23 10:46:07 +0300

    Fix ConcurrentModificationException in SnapshotDependenciesChecker (#819)
    * Fix ConcurrentModificationException
    
    * Fix ConcurrentModificationException
    
    ---------
    
    Co-authored-by: saukhmv <saukhmv@nspk.ru>

--- src/main/groovy/pl/allegro/tech/build/axion/release/domain/SnapshotDependenciesChecker.groovy
+++ src/main/groovy/pl/allegro/tech/build/axion/release/domain/SnapshotDependenciesChecker.groovy
@@ -1,20 +1,27 @@
 package pl.allegro.tech.build.axion.release.domain
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.DependencyConstraint
 
 class SnapshotDependenciesChecker {
 
     Collection<String> snapshotVersions(Project project) {
-        Collection<String> projectVersions = project.rootProject.allprojects.collect {toFullVersion(it)}
-        Collection<String> allDependenciesVersions = project.allprojects.collect {
-            it.configurations.collect { config ->
-                config.allDependencies.findAll {isSnapshot(it)}.collect {toFullVersion(it)}+
-                    config.allDependencyConstraints.findAll {isSnapshot(it)}.collect {toFullVersion(it)}
-
-            }
-        }.flatten().unique()
+        Collection<String> projectVersions = project.getRootProject().getAllprojects()
+            .collect { toFullVersion(it) }
+        Collection<Configuration> configurations = project.getRootProject().getAllprojects()
+            .collect { it.getConfigurations() }.flatten() as Collection<Configuration>
+        Collection<String> allDependenciesVersions = new HashSet<>()
+        for (Configuration config : configurations) {
+            Collection<String> versions = config.getAllDependencies()
+                .findAll { isSnapshot(it) }
+                .collect { toFullVersion(it) }
+            +config.getAllDependencyConstraints()
+                .findAll { isSnapshot(it) }
+                .collect { toFullVersion(it) }
+            allDependenciesVersions.addAll(versions)
+        }
         allDependenciesVersions.removeAll(projectVersions)
         return allDependenciesVersions
     }
@@ -22,6 +29,7 @@ class SnapshotDependenciesChecker {
     boolean isSnapshot(Dependency dependency) {
         dependency.version?.endsWith("-SNAPSHOT")
     }
+
     boolean isSnapshot(DependencyConstraint dependency) {
         dependency.version?.endsWith("-SNAPSHOT")
     }
