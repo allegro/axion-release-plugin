83e82f448a9f487e59c42c2f09a44c1e8785ffd5
Bartosz Gałek <bartosz.galek@allegro.com>
2025-08-14 11:44:08 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     signing
     jacoco
     idea
-    id("pl.allegro.tech.build.axion-release") version "1.19.1"
+    id("pl.allegro.tech.build.axion-release") version "1.20.0"
     id("com.gradle.plugin-publish") version "1.3.1"
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     id("com.coditory.integration-test") version "1.5.1"
